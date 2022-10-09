324d209e5e26a9aadd8c808281013bfb969b29fd
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2022-10-09 17:24:19 +0400

    fix grade-system

--- assets/scripts/stars.js
+++ assets/scripts/stars.js
@@ -7,15 +7,17 @@ class Stars {
         console.log(mark)
 
         for (let i = 0; i < 4; i++) {
-            if (mark % 1 === 0) {
-                let $starFull = makeElem('span', 'material-icons', 'star');
-                $starsCNR.appendChild($starFull);
-                mark -= 0.5;
-            } else if (mark % 1 == 0.5) {
+            if (mark % 1 == 0.5 && mark > 0) {
                 let $starHalf = makeElem('span', 'material-icons', 'star_half');
                 $starsCNR.appendChild($starHalf);
                 mark -= 0.5;
-            } else if (mark === 0 || mark < 0) {
+            }
+            else if (mark <= 8 && mark > 0) {
+                let $starFull = makeElem('span', 'material-icons', 'star');
+                $starsCNR.prepend($starFull);
+                mark -= 1;
+            }
+            else if (mark <= 0) {
                 let $starVoid = makeElem('span', 'material-icons', 'star_border');
                 $starsCNR.appendChild($starVoid);
             }
